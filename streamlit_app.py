88787afef8afd500e5434fe3bfd4c479429c4348
Sk. Ferdous Hossain <50297388+Ferdous52@users.noreply.github.com>
2026-09-06 23:45:11 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,21 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+    st.markdown("""
+    <style>
 
+    .stApp {background:linear-gradient(135deg,
+                                       #0F172A 0%,
+                                       #172554 50%,
+                                       #0F172A 100%)!important;
+        }
+        
+    .main .block-container {min-height: 100vh;
+                            box-sizing: border-box;
+                            display: flex;
+                            flex-direction: column;
+                            justify-content: center;
+                            padding-top: 30px !important;
+                            padding-bottom: 30px !important;}
+    </style>
+    """, unsafe_allow_html=True)
