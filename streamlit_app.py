c074ff3f06f3aa6122ed49d2232e30dd4e0e3aaf
Sk. Ferdous Hossain <50297388+Ferdous52@users.noreply.github.com>
2026-09-16 23:41:25 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-
-# ============================================================
-# PAGE CONFIG
-# ============================================================
+# =========================================================
+# PAGE CONFIGURATION
+# =========================================================
 
 st.set_page_config(
     page_title="PIM Dashboard",
@@ -12,20 +11,11 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-st.markdown("""
-    <style>
+def page_style():
+    return  ("""<style>.stApp{background: #2F172F;}.stSelectbox{background: #8F172A;}</style>""")
 
-    .stApp {background:linear-gradient(135deg,
-                                       #0F172A 0%,
-                                       #172554 50%,
-                                       #0F172A 100%)!important;
-        }
-        
-    .main .block-container {min-height: 100vh;
-                            box-sizing: border-box;
-                            display: flex;
-                            flex-direction: column;
-                            justify-content: center;
-                            padding-top: 30px !important;
-                            padding-bottom: 30px !important;}
-    </style>""", unsafe_allow_html=True)
+
+
+st.markdown(page_style(),unsafe_allow_html=True)
+
+st.selectbox("Label",options=("Option 1", "Option 2", "Option 3"))
