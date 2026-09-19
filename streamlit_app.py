f80e89132e2be4476532fffcc55ed2fc9f3c8a4e
Sk. Ferdous Hossain <50297388+Ferdous52@users.noreply.github.com>
2026-09-19 10:18:15 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,21 @@ import streamlit as st
 
 
 def page_setup():
-    page_setup = (page_title="PIM Dashboard",page_icon="📊",layout="wide",initial_sidebar_state="collapsed")
+   page_setup {"page_title": "PIM Dashboard","page_icon": "📊","layout": "wide","initial_sidebar_state": "collapsed"}
     return page_setup
-    
+
+
 def page_style():
-    page = ("""<style>.stApp{background: #2F172F;}</style>""")
-    return  page
+   page_style = ( """<style>.stApp {background: #2F172F;}</style>"""
+    return page_style
 
 
-st.set_page_config(page_setup)
-st.markdown(page_style(),unsafe_allow_html=True)
+st.set_page_config(**page_setup())
+st.markdown(page_style(), unsafe_allow_html=True)
+
 st.balloons()
+
 on = st.toggle("Activate feature")
 
-
-
+if on:
+    st.write("Feature activated!")
