a0dcfcfc9eed78451705e24b6d7279a10ed5a2db
Sk. Ferdous Hossain <50297388+Ferdous52@users.noreply.github.com>
2026-09-19 10:22:39 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def page_style():
    page_style = ("""\<style>.stApp{background: #2F172F;}\</style>""")
-    return page_style
+   return page_style
 
 st.set_page_config(page_title="PIM Dashboard",page_icon="📊",layout="wide",initial_sidebar_state="collapsed")
 st.markdown(page_style(), unsafe_allow_html=True)
