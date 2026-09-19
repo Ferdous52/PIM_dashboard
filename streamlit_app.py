8c7369c0c49fb5f4267c3044cdc14638907900cc
Sk. Ferdous Hossain <50297388+Ferdous52@users.noreply.github.com>
2026-09-19 10:19:37 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,12 @@
 import streamlit as st
 
 
-def page_setup():
-   page_setup {"page_title": "PIM Dashboard","page_icon": "📊","layout": "wide","initial_sidebar_state": "collapsed"}
-    return page_setup
-
 
 def page_style():
    page_style = ( """<style>.stApp {background: #2F172F;}</style>"""
     return page_style
 
-
-st.set_page_config(**page_setup())
+st.set_page_config(page_title="PIM Dashboard",page_icon="📊",layout="wide",initial_sidebar_state="collapsed")
 st.markdown(page_style(), unsafe_allow_html=True)
 
 st.balloons()
